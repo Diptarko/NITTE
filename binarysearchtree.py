04742e89c736c406417d7f96d6f58fc9435d98b8
Diptarko <60255975+Diptarko@users.noreply.github.com>
2024-06-15 09:41:40 +0530

    Updated Binary search tree
    Updated Binary search tree with delete node function.

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -53,6 +53,53 @@ class BinarySearchTree():
 
             else:
                 return False #Nothing found
+    
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+
+    def find_min(self):
+        # The minimum value in a BST is the leftmost node.
+        if self.left is None:
+            return self.data
+        # Recursively go to the left to find the smallest value.
+        return self.left.find_min()
+    
+    def delete(self, val):
+        # If the value to be deleted is less than the current node's value,
+        # then it lies in the left subtree.
+        if val < self.data:
+            if self.left:
+                # Recursively call delete on the left subtree.
+                self.left = self.left.delete(val)
+        # If the value to be deleted is greater than the current node's value,
+        # then it lies in the right subtree.
+        elif val > self.data:
+            if self.right:
+                # Recursively call delete on the right subtree.
+                self.right = self.right.delete(val)
+        # If the value is equal to the current node's value, then this is the node to be deleted.
+        else:
+            # Case 1: Node with no children (leaf node).
+            if self.left is None and self.right is None:
+                return None
+            # Case 2: Node with only one child (right child).
+            elif self.left is None:
+                return self.right
+            # Case 2: Node with only one child (left child).
+            elif self.right is None:
+                return self.left
+
+            # Case 3: Node with two children.
+            # Find the inorder successor (smallest in the right subtree).
+            min_val = self.right.find_min()
+            # Replace the data of this node with the inorder successor's data.
+            self.data = min_val
+            # Delete the inorder successor.
+            self.right = self.right.delete(min_val)
+
+        return self
 
 #Helper method
 def build_tree(elements):
@@ -68,6 +115,10 @@ if __name__ =='__main__':
     numbers_tree = build_tree(elements)
     print(numbers_tree.in_order_traversal())
     print(numbers_tree.search(25))
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree.delete(20)
+
+    print("After deleting 20 ",numbers_tree.in_order_traversal()) # this should print [1, 4, 9, 17, 18, 23, 34]
     countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
     country_tree = build_tree(countries)
     print(country_tree.in_order_traversal())
